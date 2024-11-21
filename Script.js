03a9c47b339383e864a6634b7bd6776160582067
BEEL <montesluis877@gmail.com>
2024-11-21 13:13:56 -0500

    OrdenarCodigo

--- Script.js
+++ Script.js
@@ -1,28 +1,29 @@
 function Persona(nombre, telefono, correo, etiqueta) {
-    this.nombre = nombre;
-    this.telefono = telefono;
-    this.correo = correo;
-    this.etiqueta = etiqueta;
-  }
-  
-  let ListaContacto = [];
+  this.nombre = nombre;
+  this.telefono = telefono;
+  this.correo = correo;
+  this.etiqueta = etiqueta;
+}
 
-  function GuardarPersona() {
-    ListaContacto.push(new Persona(
-  
-        document.getElementById("Nombre").value,
-        document.getElementById("Telefono").value,
-        document.getElementById("Correo").value,
-        document.getElementById("inputGroupSelect01").value
-    ));
-    mostrarLista();
-  }
-  
-  function mostrarLista() {
-    var todo = document.getElementById("llenar");
-    todo.innerHTML = "";
-    ListaContacto.forEach((pers,index)=> {
-        todo.innerHTML += `
+let ListaContacto = [];
+
+function GuardarPersona() {
+  ListaContacto.push(
+    new Persona(
+      document.getElementById("Nombre").value,
+      document.getElementById("Telefono").value,
+      document.getElementById("Correo").value,
+      document.getElementById("inputGroupSelect01").value
+    )
+  );
+  mostrarLista();
+}
+
+function mostrarLista() {
+  var todo = document.getElementById("llenar");
+  todo.innerHTML = "";
+  ListaContacto.forEach((pers, index) => {
+    todo.innerHTML += `
          <tr>
                     <td>${pers.nombre}</td>
                     <td>${pers.telefono}</td>
@@ -36,24 +37,27 @@ function Persona(nombre, telefono, correo, etiqueta) {
                     </td>
                 </tr>
                 `;
-            });
-           
-        }
-        function Eliminar(index){
-            ListaContacto.splice(index, 1);
-            mostrarLista();
-        }
-  
-        function Editar(index){
-  
-            ListaContacto.splice(index, 1, { nombre:document.getElementById("nombreEd").value, telefono:document.getElementById("telefonoed").value ,correo: document.getElementById("correoed").value , etiqueta:document.getElementById("inputGroupSelect01ed").value  });
-           mostrarLista();
-       }
-  
-       function alertasss(id){
-       Swal.fire({
-        title: 'Ingrese sus datos',
-        html: `
+  });
+}
+function Eliminar(index) {
+  ListaContacto.splice(index, 1);
+  mostrarLista();
+}
+
+function Editar(index) {
+  ListaContacto.splice(index, 1, {
+    nombre: document.getElementById("nombreEd").value,
+    telefono: document.getElementById("telefonoed").value,
+    correo: document.getElementById("correoed").value,
+    etiqueta: document.getElementById("inputGroupSelect01ed").value,
+  });
+  mostrarLista();
+}
+
+function alertasss(id) {
+  Swal.fire({
+    title: "Ingrese sus datos",
+    html: `
             <input id="nombreEd" class="swal2-input" placeholder="Nombre">
             <input id="telefonoed" class="swal2-input" placeholder="Telefono">
             <input id="correoed" class="swal2-input" placeholder="Correo">
@@ -65,38 +69,35 @@ function Persona(nombre, telefono, correo, etiqueta) {
                                 </select>
                             </div>
         `,
-        title: "Quieres Realizar Cambios",
-        showDenyButton: true,
-        showCancelButton: false,
-        confirmButtonText: "Guardar",
-        denyButtonText: "Cancelar"
-      }).then((result) => {
-        if (result.isConfirmed) {
-            Editar(id);
-          Swal.fire("Saved!", "", "success");
-        } else if (result.isDenied) {
-          Swal.fire("Changes are not saved", "", "info");
-        }
-      });}
-       
-       function etiqueta(){
-        return document.getElementById("inputGroupSelect01fl").value
-      }
-
-      function mostrartabla(){
-
-        return  document.getElementById("flitrador").value;
-    
+    title: "Quieres Realizar Cambios",
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: "Guardar",
+    denyButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Editar(id);
+      Swal.fire("Saved!", "", "success");
+    } else if (result.isDenied) {
+      Swal.fire("Changes are not saved", "", "info");
     }
+  });
+}
 
-   
-    document.getElementById("btn").addEventListener("click" , function(){
-    var todo =  document.getElementById("llenar");
-    todo.innerHTML ="";
-    if(mostrartabla()==="" && etiqueta()===""){
-    
-      ListaContacto.forEach((pers,index)=>{
-            todo.innerHTML += `
+function etiqueta() {
+  return document.getElementById("inputGroupSelect01fl").value;
+}
+
+function mostrartabla() {
+  return document.getElementById("flitrador").value;
+}
+
+document.getElementById("btn").addEventListener("click", function () {
+  var todo = document.getElementById("llenar");
+  todo.innerHTML = "";
+  if (mostrartabla() === "" && etiqueta() === "") {
+    ListaContacto.forEach((pers, index) => {
+      todo.innerHTML += `
          <tr>
                     <td>${pers.nombre}</td>
                     <td>${pers.telefono}</td>
@@ -110,15 +111,18 @@ function Persona(nombre, telefono, correo, etiqueta) {
                     </td>
                 </tr>
                 `;
-        } )
-    
-    }else{
-       
-        var filtroo = ListaContacto.filter(perss =>  perss.nombre === mostrartabla()  || perss.etiqueta === etiqueta());
-        filtroo.forEach((flt) =>{
-          const originalIndex = ListaContacto.findIndex(pers => pers.nombre === flt.nombre);
-          const filteredIndex = ListaContacto.indexOf(flt);
-    todo.innerHTML += `
+    });
+  } else {
+    var filtroo = ListaContacto.filter(
+      (perss) =>
+        perss.nombre === mostrartabla() || perss.etiqueta === etiqueta()
+    );
+    filtroo.forEach((flt) => {
+      const originalIndex = ListaContacto.findIndex(
+        (pers) => pers.nombre === flt.nombre
+      );
+      const filteredIndex = ListaContacto.indexOf(flt);
+      todo.innerHTML += `
          <tr>
                     <td>${flt.nombre}</td>
                     <td>${flt.telefono}</td>
@@ -132,12 +136,6 @@ function Persona(nombre, telefono, correo, etiqueta) {
                     </td>
                 </tr>
                 `;
-    
-        })
-    
-    
-        
-    }})
-    
-    
-    
+    });
+  }
+});
